4c8b0eacdd7426e878ee3bd874aa0402a7eb1e59
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-03-26 16:19:01 +0100

    constants for action name

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,5 +1,7 @@
+import { CREATE_COURSE } from '../actions/actionTypes';
+
 export default function courseReducer(state = [], action) {
-    if (action.type === 'CREATE_COURSE') {
+    if (action.type === CREATE_COURSE) {
         return [...state, { ...action.course }];
     }
     return state;
