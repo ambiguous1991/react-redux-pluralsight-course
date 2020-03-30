246b8c980f43bea194fd655480aa9e476a005e85
Jakub Bartusiak <jakub.bartusiak@gmail.com>
2020-03-30 10:17:01 +0200

    Production build

---
+++ src/redux/configureStore.dev.js
@@ -0,0 +1,18 @@
+import { applyMiddleware, compose, createStore } from 'redux';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import rootReducer from './reducers';
+import thunk from 'redux-thunk';
+
+export default function configureStoreDev(initialState) {
+    const composeEnhancers =
+        typeof window === 'object' &&
+        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+            : compose;
+
+    return createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
+    );
+}
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,18 +1,5 @@
-import { applyMiddleware, compose, createStore } from 'redux';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
-import rootReducer from './reducers';
-import thunk from 'redux-thunk';
-
-export default function configureStore(initialState) {
-    const composeEnhancers =
-        typeof window === 'object' &&
-        window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-            ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
-            : compose;
-
-    return createStore(
-        rootReducer,
-        initialState,
-        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
-    );
+if (process.env.NODE_ENV === 'production') {
+    module.exports = require('./configureStore.prod');
+} else {
+    module.exports = require('./configureStore.dev');
 }
